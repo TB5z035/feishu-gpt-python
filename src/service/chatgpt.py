09ac6b197c24362b93dd4bb337b3676c6e93e937
Beiwen Tian <tianbeiwen88@gmail.com>
2024-09-17 03:54:04 +0000

    fix openai v1 api

--- src/service/chatgpt.py
+++ src/service/chatgpt.py
@@ -1,17 +1,14 @@
 # Note: The openai-python library support for Azure OpenAI is in preview.
 import logging
 import os
-import openai
+from openai import AzureOpenAI
 from util.app_config import app_config
 from util.logger import gpt_logger, app_logger
 if app_config.IS_AZURE:
-    openai.api_type = "azure"
-    openai.api_base = app_config.AZURE_API_HOST
-    openai.api_version = "2023-03-15-preview"
-    openai.api_key = app_config.AZURE_API_KEY
-else:
-    openai.api_key = app_config.OPENAI_KEY
-
+    client = AzureOpenAI(azure_endpoint=app_config.AZURE_API_HOST,
+        api_version="2023-12-01-preview",
+        api_key=app_config.AZURE_API_KEY,
+        api_key=app_config.OPENAI_KEY)
 
 def get_single_response(message, prompt=app_config.DEFAULT_PROMPT):
     return get_chat_response([{"role": "user", "content": message}])
@@ -24,7 +21,7 @@ def get_chat_response(chat_history, prompt=app_config.DEFAULT_PROMPT):
     if "choices" not in response:
         gpt_logger.info("GPT raw response: %s", response)
         return ""
-    choice = response["choices"][0]  # type: ignore
+    choice = response.choices[0]  # type: ignore
     if "message" not in choice:
         gpt_logger.info("GPT raw response: %s", response)
         return ""
@@ -37,8 +34,7 @@ def get_chat_response(chat_history, prompt=app_config.DEFAULT_PROMPT):
 
 
 def get_gpt_response(messages):
-    response = openai.ChatCompletion.create(
-        engine=app_config.GPT_MODEL,
+    response = client.chat.completions.create(model=app_config.GPT_MODEL,
         messages=messages,
         stop=None)
     return response
